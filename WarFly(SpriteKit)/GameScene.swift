f6039fba7c36390882782a0d2243a01246dfc0ef
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-13 14:12:27 +0300

    add Collisions

--- WarFly(SpriteKit)/GameScene.swift
+++ WarFly(SpriteKit)/GameScene.swift
@@ -157,10 +157,24 @@ class GameScene: SKScene {
 extension GameScene: SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
-        print("contact detected")
+        
+        let bodyA = contact.bodyA.categoryBitMask
+        let bodyB = contact.bodyB.categoryBitMask
+        let player = BitMaskCategory.player
+        let enemy = BitMaskCategory.enemy
+        let shot = BitMaskCategory.shot
+        let powerUp = BitMaskCategory.powerUp
+        
+        if bodyA == player && bodyB == enemy || bodyB == player && bodyA == enemy {
+            print("enemy vc playes")
+        } else if bodyA == player && bodyB == powerUp || bodyB == player && bodyA == powerUp {
+            print("powerUp vs playes")
+        } else if bodyA == shot && bodyB == enemy || bodyB == shot && bodyA == enemy {
+            print("enemy vs shot")
+        }
     }
     
     func didEnd(_ contact: SKPhysicsContact) {
-        print("asdascontact detected")
+       
     }
 }
