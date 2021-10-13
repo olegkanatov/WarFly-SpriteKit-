39e4ff4cc9556562fb925f410f85f978b760bf3d
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-13 15:24:30 +0300

    make it nice

--- WarFly(SpriteKit)/Scenes/GameScene.swift
+++ WarFly(SpriteKit)/Scenes/GameScene.swift
@@ -11,14 +11,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var player: PlayerPlane!
-    let scoreBackground = SKSpriteNode(imageNamed: "scores")
-    let scoreLabel = SKLabelNode(text: "10000")
-    let menuButton = SKSpriteNode(imageNamed: "menu")
-    let life1 = SKSpriteNode(imageNamed: "life")
-    let life2 = SKSpriteNode(imageNamed: "life")
-    let life3 = SKSpriteNode(imageNamed: "life")
-    
+    fileprivate var player: PlayerPlane!
+    fileprivate let hud = HUD()
+    fileprivate let screenSize = UIScreen.main.bounds.size
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
@@ -31,35 +26,12 @@ class GameScene: SKScene {
         
         spawnPowerUp()
         spawnEnemies()
-        configureUI()
+        createHud()
     }
     
-    fileprivate func configureUI() {
-        scoreBackground.position = CGPoint(x: scoreBackground.size.width + 10, y: self.size.height - scoreBackground.size.height / 2  - 10)
-        scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
-        scoreBackground.zPosition = 99
-        addChild(scoreBackground)
-        
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.verticalAlignmentMode = .center
-        scoreLabel.position = CGPoint(x: -10, y: 3)
-        scoreLabel.zPosition = 100
-        scoreLabel.fontName = "AmericanTypewritter-Bold"
-        scoreLabel.fontSize = 30
-        scoreBackground.addChild(scoreLabel)
-        
-        menuButton.position = CGPoint(x: 20, y: 20)
-        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-        menuButton.zPosition = 100
-        addChild(menuButton)
-        
-        let lifes = [life1, life2, life3]
-        for (index, life) in lifes.enumerated() {
-            life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 20)
-            life.zPosition = 100
-            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-            addChild(life)
-        }
+    fileprivate func createHud() {
+        addChild(hud)
+        hud.configureUI(screenSize: screenSize)
     }
     
     fileprivate func spawnPowerUp() {
